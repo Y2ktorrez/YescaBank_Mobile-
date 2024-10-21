29eff726c66a0eb7236b3529ff2e533c669f0fb5
Aaron-Martinez-Balderrama-2 <a3ronmartinez@gmail.com>
2024-10-21 01:54:26 -0300

    mas diseño visual

--- lib/screens/add_account.dart
+++ lib/screens/add_account.dart
@@ -9,20 +9,21 @@ class AddAccountPage extends StatefulWidget {
   State<AddAccountPage> createState() => _AddAccountPageState();
 }
 
-class _AddAccountPageState extends State<AddAccountPage> {
+class _AddAccountPageState extends State<AddAccountPage>
+    with SingleTickerProviderStateMixin {
   final AccountService _accountService = AccountService();
   final TextEditingController _ciController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
+  String _selectedCurrency = "USD";
+  String _selectedAccountType = "Cuenta Corriente";
 
-  String _selectedCurrency = "USD"; // Moneda predeterminada
-  String _selectedAccountType = "Cuenta Corriente"; // Tipo de cuenta predeterminada
+  late AnimationController _animationController;
+  late Animation<double> _buttonScaleAnimation;
 
   // Método para registrar la cuenta
   Future<void> _createAccount() async {
     if (_ciController.text.isEmpty || _emailController.text.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Por favor, completa todos los campos.')),
-      );
+      _showSnackBar('Por favor, completa todos los campos.');
       return;
     }
 
@@ -37,10 +38,36 @@ class _AddAccountPageState extends State<AddAccountPage> {
       await _accountService.createAccount(account);
       Navigator.pop(context);
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error: $e')),
-      );
+      _showSnackBar('Error: $e');
     }
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.redAccent,
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _buttonScaleAnimation = Tween<double>(begin: 1.0, end: 1.1).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -48,20 +75,20 @@ class _AddAccountPageState extends State<AddAccountPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Crear Nueva Cuenta'),
-        backgroundColor: Colors.teal,
+        backgroundColor: const Color.fromARGB(255, 2, 143, 148), 
+        elevation: 8.0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+        ),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            // Título
-            const Text(
-              'Completa los detalles de la cuenta',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
             const SizedBox(height: 20),
+            _buildAnimatedTitle(),
+            const SizedBox(height: 30),
 
             // Campo CI
             _buildTextField(
@@ -102,20 +129,29 @@ class _AddAccountPageState extends State<AddAccountPage> {
                 });
               },
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 40),
 
-            // Botón de Crear Cuenta
-            ElevatedButton(
-              onPressed: _createAccount,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal, // Cambiado de 'primary' a 'backgroundColor'
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              child: const Text('Crear Cuenta'),
-            ),
+            // Botón de Crear Cuenta con animación
+            _buildAnimatedButton(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildAnimatedTitle() {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+      child: const Text(
+        'Completa los detalles de la cuenta',
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.5,
+          color: Color.fromARGB(255, 0, 0, 0),
+        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
@@ -130,11 +166,14 @@ class _AddAccountPageState extends State<AddAccountPage> {
       keyboardType: keyboardType,
       decoration: InputDecoration(
         labelText: label,
-        border: OutlineInputBorder(),
+        labelStyle: const TextStyle(color: Color.fromARGB(255, 7, 7, 7)),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
         filled: true,
-        fillColor: Colors.grey[200 ],
+        fillColor: const Color(0xFFEDEDED), // Gris claro
       ),
-      cursorColor: Colors.teal,
+      cursorColor: const Color(0xFF003366),
     );
   }
 
@@ -147,18 +186,57 @@ class _AddAccountPageState extends State<AddAccountPage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Text(label),
-        DropdownButton<String>(
-          value: value,
-          onChanged: onChanged,
-          items: items.map((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+        Text(
+          label,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: const Color(0xFF003366)),
+          ),
+          child: DropdownButton<String>(
+            value: value,
+            onChanged: onChanged,
+            isExpanded: true,
+            underline: const SizedBox(),
+            items: items.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
         ),
       ],
+    );
+  }
+
+  Widget _buildAnimatedButton() {
+    return ScaleTransition(
+      scale: _buttonScaleAnimation,
+      child: ElevatedButton(
+        onPressed: () {
+          _animationController.forward().then((_) {
+            _animationController.reverse();
+          });
+          _createAccount();
+        },
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          backgroundColor: const Color.fromARGB(255, 2, 143, 148), // Verde esmeralda
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          elevation: 6.0,
+        ),
+        child: const Text(
+          'Crear Cuenta',
+          style: TextStyle(fontSize: 20, letterSpacing: 1.5, color: Color.fromARGB(255, 7, 7, 7)),
+        ),
+      ),
     );
   }
 }
